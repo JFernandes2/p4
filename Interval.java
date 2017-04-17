644da1713437afc093556e235ff53c0af32b67df
Aseeel <aseel.beshri@gmail.com>
2017-04-16 21:17:30 -0500

    Update Interval.java

--- Interval.java
+++ Interval.java
@@ -2,39 +2,52 @@
 public class Interval<T extends Comparable<T>> implements IntervalADT<T> {
 
     // TODO declare any needed data members
+	private T start;
+	private T end;
+	private String label;
 
     public Interval(T start, T end, String label) {
-        // TODO Auto-generated constructor stub
+    	this.start = start;
+    	this.end = end;
+    	this.label = label;
     }
 
     @Override
     public T getStart() {
-        // TODO Auto-generated method stub
+        return start;
     }
 
     @Override
     public T getEnd() {
-        // TODO Auto-generated method stub
+        return end;
     }
 
     @Override
     public String getLabel() {
-        // TODO Auto-generated method stub
+        return label;
     }
 
     @Override
     public boolean overlaps(IntervalADT<T> other) {
-        // TODO Auto-generated method stub
+    	if (this.getEnd().compareTo(other.getStart()) < 0 || other.getEnd().compareTo(this.getStart()) < 0)
+    		return false;
+    	return true;
     }
 
     @Override
     public boolean contains(T point) {
-        // TODO Auto-generated method stub
+    	if (point.compareTo(this.getStart()) >= 0 && point.compareTo(this.getEnd()) <= 0)
+    		return true;
+    	return false;
     }
 
     @Override
     public int compareTo(IntervalADT<T> other) {
-        // TODO Auto-generated method stub
+        if (other.getStart().compareTo(this.getStart())
+    }
+    @Override
+	public String toString()  {
+    	
     }
 
 }
